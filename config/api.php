4eb251c709c8af8ef23528f13444103d67915c2c
BerkanYesilyurt <berkan38212@gmail.com>
2023-08-08 21:35:27 +0300

    standardized api config

--- config/api.php
+++ config/api.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
-    'the_movie_db_api_key' => env('the_movie_db_api_key'),
+    'tmdb' => [
+        'base_url' => 'https://api.themoviedb.org/3/',
+        'api_key' => env('the_movie_db_api_key'),
+        'timeout' => 5,
+        'paths' => [
+            'authentication' => 'authentication',
+            'movie_genres' => 'genre/movie/list',
+            'tv_series_genres' => 'genre/tv/list'
+        ]
+    ]
 ];
