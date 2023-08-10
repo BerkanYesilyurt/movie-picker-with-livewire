28b69a30a66dd701c4aa738310ccfa7f5620b94e
BerkanYesilyurt <berkan38212@gmail.com>
2023-08-10 21:08:04 +0300

    prepareTheRequest & generateTheUrl & checkAuth funcs

--- app/Services/TMDB/Service.php
+++ app/Services/TMDB/Service.php
@@ -3,7 +3,10 @@
 namespace App\Services\TMDB;
 
 use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Fluent;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class Service
 {
@@ -19,4 +22,29 @@ class Service
         $this->request = $this->prepareTheRequest();
     }
 
+    private function prepareTheRequest(): PendingRequest
+    {
+        return Http::withToken($this->apiKey)
+            ->withoutVerifying()->withHeaders([
+                'Accept' => 'application/json'
+            ])->timeout($this->timeout);
+    }
+
+    private function generateTheUrl($path): string|bool
+    {
+        return isset($path)
+            ? $this->baseUrl . $path
+            : false;
+    }
+
+    public function checkAuth(): bool
+    {
+        $response = $this->request->get(
+            url: $this->generateTheUrl($this->paths->authentication)
+        );
+
+        return !$response->failed()
+            && $response->status() == ResponseAlias::HTTP_OK
+            && $response->json('success') == true;
+    }
 }
