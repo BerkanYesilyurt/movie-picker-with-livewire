1312f8b32a69f33a68498d4d8cc4e1d6ec971aa7
BerkanYesilyurt <berkan38212@gmail.com>
2023-08-13 16:03:18 +0300

    index route defined

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomePageController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
