0d816e50f993a49de69229426a8df873aed5fcde
BerkanYesilyurt <berkan38212@gmail.com>
2023-08-18 18:32:32 +0300

    tmdbService defined in construct

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Services\TMDB\Service;
-use Illuminate\Http\Request;
 
 class HomePageController extends Controller
 {
+    public function __construct()
+    {
+        $this->tmdbService = resolve(Service::class);
+    }
+
     public function index()
     {
-        $tmdbService = resolve(Service::class);
-        if($tmdbService->checkAuth()){
+        if($this->tmdbService->checkAuth()){
             return view('pages.homepage', [
-                'movie_genres' => $tmdbService->getData('movie_genres', 'genres', ['id', 'name']),
-                'tv_series_genres' => $tmdbService->getData('tv_series_genres', 'genres', ['id', 'name'])
+                'movie_genres' => $this->tmdbService->getData('movie_genres', 'genres', ['id', 'name']),
+                'tv_series_genres' => $this->tmdbService->getData('tv_series_genres', 'genres', ['id', 'name'])
             ]);
         }
     }
