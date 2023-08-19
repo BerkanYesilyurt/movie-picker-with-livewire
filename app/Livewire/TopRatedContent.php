6032a176c9ef3a5cf4f0e9e787f79219e2414537
BerkanYesilyurt <berkan38212@gmail.com>
2023-08-19 14:16:56 +0300

    content added into component

--- app/Livewire/TopRatedContent.php
+++ app/Livewire/TopRatedContent.php
@@ -6,5 +6,15 @@ use Livewire\Component;
 
 class TopRatedContent extends Component
 {
+    public $content;
 
+    public function mount($content = NULL)
+    {
+        $this->content = $content;
+    }
+
+    public function render()
+    {
+        return view('livewire.top-rated-content');
+    }
 }
