c3e04b53187266ae9059511fced15bde29255df4
BerkanYesilyurt <berkan38212@gmail.com>
2023-08-20 19:08:02 +0300

    get data of top rated content & minor changes

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -15,8 +15,23 @@ class HomePageController extends Controller
     {
         if($this->tmdbService->checkAuth()){
             return view('pages.homepage', [
-                'movie_genres' => $this->tmdbService->getData('movie_genres', 'genres', ['id', 'name']),
-                'tv_series_genres' => $this->tmdbService->getData('tv_series_genres', 'genres', ['id', 'name'])
+                'movie_genres' => $this->tmdbService->getData(
+                    path: 'movie_genres',
+                    responseKey: 'genres',
+                    params: ['id', 'name']),
+                'tv_series_genres' => $this->tmdbService->getData(
+                    path: 'tv_series_genres',
+                    responseKey: 'genres',
+                    params:['id', 'name']),
+                'movie_top_rated' => $this->tmdbService->getData(
+                    path: 'movie_top_rated',
+                    responseKey: 'results',
+                    params: ['id', 'genre_ids', 'poster_path', 'title', 'vote_average']),
+                'tv_series_top_rated' => $this->tmdbService->getData(
+                    path: 'tv_series_top_rated',
+                    responseKey: 'results',
+                    params: ['id', 'genre_ids', 'poster_path', 'name', 'vote_average'],
+                    queryParams: ['language' => 'en-US', 'page' => 1])
             ]);
         }
     }
