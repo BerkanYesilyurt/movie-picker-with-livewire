9f244b7497787d79b6214c40840feb023e5c8411
BerkanYesilyurt <berkan38212@gmail.com>
2023-08-21 19:56:26 +0300

    call the component for every content

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,2 +1,12 @@
 <livewire:genres :genres="$movie_genres" />
 <livewire:genres :genres="$tv_series_genres" />
+
+@foreach($movie_top_rated as $movie)
+    <br><br>
+    <livewire:top-rated-content :content="$movie" :genre="$movie_genres" />
+@endforeach
+
+@foreach($tv_series_top_rated as $tv_series)
+    <br><br>
+    <livewire:top-rated-content :content="$tv_series" :genre="$tv_series_genres" />
+@endforeach
