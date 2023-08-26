e6bba8750ed9bf9fef48d4be95f13347f1117827
BerkanYesilyurt <berkan38212@gmail.com>
2023-08-26 17:30:47 +0300

    remove has control in remember helper

--- app/Helpers/general.php
+++ app/Helpers/general.php
@@ -12,9 +12,7 @@ if(!function_exists('checkKeysFromResult')){
 if(!function_exists('remember')){
     function remember(string $key, int $seconds = 300, $model = null, $data = null)
     {
-        if(cache()->has($key))
-            return cache()->get($key);
-        elseif($model || $data)
+        if($model || $data)
             return cache()->remember($key, $seconds, function () use ($data, $model) {
                 return $model ? app("App\\Models\\$model")->get() : $data;
             });
