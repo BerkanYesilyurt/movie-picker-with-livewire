c20b9f23cdd7f356f6fa10d2b62b1ed876508a9a
BerkanYesilyurt <berkan38212@gmail.com>
2023-08-30 19:29:31 +0300

    tv & movie discover paths

--- config/api.php
+++ config/api.php
@@ -10,7 +10,9 @@ return [
             'movie_genres' => 'genre/movie/list',
             'tv_series_genres' => 'genre/tv/list',
             'movie_top_rated' => 'movie/top_rated',
-            'tv_series_top_rated' => 'tv/top_rated'
+            'tv_series_top_rated' => 'tv/top_rated',
+            'movie_discover' => 'discover/movie',
+            'tv_series_discover' => 'discover/tv',
         ]
     ]
 ];
