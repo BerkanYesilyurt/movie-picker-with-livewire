678d43d2fc7104870002dc0620cba264e8f63bb6
BerkanYesilyurt <berkan38212@gmail.com>
2023-09-02 22:26:00 +0300

    vote average & vote count & adult fields

--- resources/views/livewire/content-suggestion.blade.php
+++ resources/views/livewire/content-suggestion.blade.php
@@ -7,10 +7,24 @@
 
     <form wire:submit.prevent="generateContent">
         <select wire:model="type" required>
-            <option value="">Choose Content Type</option>
+            <option value="">Content Type</option>
             <option value="movie">Movie</option>
             <option value="tv">TV Series</option>
         </select>
+        <select wire:model="adult" required>
+            <option value="">Include Adult</option>
+            <option value="1">Yes</option>
+            <option value="0">No</option>
+        </select>
+        <select wire:model="vote_average" required>
+            <option value="">Vote Average</option>
+            <option value="9">> 9</option>
+            <option value="8">> 8</option>
+            <option value="7">> 7</option>
+            <option value="6">> 6</option>
+            <option value="5">> 5</option>
+        </select>
+        <input wire:model="vote_count" type="text" required placeholder="Total Vote Count"/>
         <button type="submit">OK</button>
     </form>
 </div>
