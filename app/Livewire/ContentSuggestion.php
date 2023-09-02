344a91adfb8349ccd24c8712e3023457fabbd231
BerkanYesilyurt <berkan38212@gmail.com>
2023-09-02 22:25:33 +0300

    validate other fields

--- app/Livewire/ContentSuggestion.php
+++ app/Livewire/ContentSuggestion.php
@@ -6,9 +6,12 @@ use Livewire\Component;
 
 class ContentSuggestion extends Component
 {
-    public $type;
-    protected $rules = [
+    public $type, $adult, $vote_average, $vote_count;
+    protected array $rules = [
         'type' => 'required|in:tv,movie',
+        'adult' => 'required|boolean',
+        'vote_average' => 'required|in:5,6,7,8,9',
+        'vote_count' => 'required|integer|min:1|max:100000000'
     ];
 
     public function generateContent()
