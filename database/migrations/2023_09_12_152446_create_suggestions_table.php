c33b009a92a8aee796d4be38d18362f90daa71c0
BerkanYesilyurt <berkan38212@gmail.com>
2023-09-13 20:29:29 +0300

    suggestion columns & indexes

--- database/migrations/2023_09_12_152446_create_suggestions_table.php
+++ database/migrations/2023_09_12_152446_create_suggestions_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('suggestions', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('user_id')->index();
+            $table->tinyInteger('type')->index();
+            $table->json('content');
             $table->timestamps();
         });
     }
