d4fea56ba638b68a60ecf4b49d8c4f8414a4b7e5
BerkanYesilyurt <berkan38212@gmail.com>
2023-09-14 19:59:19 +0300

    type cast

--- app/Models/Suggestion.php
+++ app/Models/Suggestion.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use App\Enums\ContentTypeEnum;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Suggestion extends Model
 {
     protected $table = 'suggestions';
+    protected $casts = [
+        'type' => ContentTypeEnum::class
+    ];
 
     public function user(): BelongsTo
     {
