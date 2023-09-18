615968bc48a620dfe9efd5e8fb1b4390580d6df6
BerkanYesilyurt <berkan38212@gmail.com>
2023-09-18 18:46:18 +0300

    user & suggestion relation

--- app/Models/UserFavorite.php
+++ app/Models/UserFavorite.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserFavorite extends Model
 {
-    use HasFactory;
+    protected $table = 'user_favorites';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function suggestion(): BelongsTo
+    {
+        return $this->belongsTo(Suggestion::class);
+    }
 }
