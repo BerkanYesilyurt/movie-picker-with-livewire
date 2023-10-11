48d5cef7157c93ac6d50c8eae13baa6c8d74c370
BerkanYesilyurt <berkan38212@gmail.com>
2023-10-11 22:00:54 +0300

    params defined

--- config/api.php
+++ config/api.php
@@ -14,6 +14,11 @@ return [
             'tv_series_top_rated' => 'tv/top_rated',
             'movie_discover' => 'discover/movie',
             'tv_series_discover' => 'discover/tv',
+        ],
+        'params' => [
+            'movie' => ['id', 'genre_ids', 'name', 'overview', 'poster_path', 'vote_average', 'vote_count', 'first_air_date'],
+            'tv_series' => ['id', 'genre_ids', 'title', 'overview', 'poster_path', 'vote_average', 'vote_count', 'release_date'],
+            'genres' => ['id', 'name']
         ]
     ]
 ];
