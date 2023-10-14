eaff28d5024c95f290eead4e0bedbefb9fd0694f
BerkanYesilyurt <berkan38212@gmail.com>
2023-10-14 19:23:28 +0300

    dashboard route

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomePageController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [HomePageController::class, 'dashboard'])->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
